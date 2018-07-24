e115b3fc68676980a001b024dbdf31e6a8ef73a2
liniabmulesoft <labraham@C02VPQHTHV29.local>
2018-07-24 12:44:04 -0400

    added cloud hub plugin

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -10,16 +10,16 @@ import org.mule.tck.junit4.rule.DynamicPort;
 public class HelloMavenTest extends FunctionalTestCase {
 	@Rule
 	public DynamicPort myPort = new DynamicPort("http.port");
-	
-    @Test
-    public void mavenFlowReturnsHelloMaven() throws Exception {
-    		System.out.println("\n\n============> TestCase#1 HTTP Dynamic port :" + myPort.getNumber() + "\n\n");
-        runFlowAndExpect("mavenFlow", "Hello Maven");
-    }
-    
-    @Override
-    protected String getConfigFile() {
-        return "maven-project.xml";
-    }
+
+	@Test
+	public void mavenFlowReturnsHelloMaven() throws Exception {
+		System.out.println("\n\n============> TestCase#1 HTTP Dynamic port :" + myPort.getNumber() + "\n\n");
+		runFlowAndExpect("mavenFlow", "Hello Maven");
+	}
+
+	@Override
+	protected String getConfigFile() {
+		return "maven-project.xml";
+	}
 
 }
